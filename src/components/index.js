c6265a992c5c871bf4691b7e023d582319d5fad1
Volodymyr <volodymyrquo@gmail.com>
2020-11-15 15:19:18 +0200

    finished particles animation

--- src/components/index.js
+++ src/components/index.js
@@ -27,6 +27,11 @@ const Home = () => {
               random: true,
               anim: { enable: true, speed: 6, size_min: 0.1, sync: true },
             },
+            opacity: {
+              value: 1,
+              random: true,
+              anim: { enable: true, speed: 1, opacity_min: 0.1, sync: true },
+            },
           },
         }}
       />
