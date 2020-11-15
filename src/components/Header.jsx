b2201301422e082c172ab6cddd0620dbcfd6b870
Volodymyr <volodymyrquo@gmail.com>
2020-11-15 15:04:29 +0200

    add animation with react-particles-js

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Box, Grid, Avatar } from '@material-ui/core';
+import avatar from '../me.JPG';
+import Typed from 'react-typed';
+
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    margin: theme.spacing(1),
+  },
+  title: { color: 'tomato' },
+  subtitle: { color: 'tan' },
+  typedContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '100vw',
+    textAlign: 'center',
+    zIndex: 1,
+  },
+}));
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.typedContainer}>
+      <Grid container justify='center'>
+        <Avatar src={avatar} alt='Volodymyr Basok' className={classes.avatar} />
+      </Grid>
+      <Typography className={classes.title} variant='h4'>
+        <Typed strings={['Volodymyr Basok']} typeSpeed={40} />
+      </Typography>
+      <br />
+      <Typography className={classes.subtitle} variant='h5'>
+        <Typed
+          strings={['React JS', 'Redux', 'Material UI']}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Header from './Header';
 import Navbar from './Navbar';
+import Particles from 'react-particles-js';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({ particlesCanva: { position: 'absolute' } });
 
 const Home = () => {
+  const classes = useStyles();
   return (
     <div>
       <Navbar />
+      <Header />
+      <Particles
+        canvasClassName={classes.particlesCanva}
+        params={{
+          particles: {
+            number: {
+              value: 45,
+
+              density: { enable: true, value_area: 900 },
+            },
+            shape: { type: 'circle', stroke: { width: 1, color: 'tomato' } },
+            size: {
+              value: 8,
+              random: true,
+              anim: { enable: true, speed: 6, size_min: 0.1, sync: true },
+            },
+          },
+        }}
+      />
     </div>
   );
 };
