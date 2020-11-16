4711a9eef92a9bd31dab65a8d932eef7af6d1ed3
Volodymyr <volodymyrquo@gmail.com>
2020-11-16 05:32:24 +0200

    finished portfolio component

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, Typography, Box, Grid, Button } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import Navbar from './Navbar';
+
+const Contacts = () => {
+  return <div></div>;
+};
+
+export default Contacts;
