1493d610be74f41e71304603122afd44810b4e6c
Volodymyr <volodymyrquo@gmail.com>
2020-12-13 09:27:26 +0200

    changes in resume

--- src/assets/utilities/workingExperience.js
+++ src/assets/utilities/workingExperience.js
@@ -13,14 +13,14 @@ export const workingExperience = [
     experience: 'Economic planning, budgeting, pricing',
   },
   {
-    year: 2011,
+    year: 2002,
     position: 'Chief of regional business development',
     company: 'INDEX Bank, Delta Bank, VAB Bank',
     experience:
       'In each position I started from scratch to develop and establish business procedures, to form the corresponding divisions and to make them perfect',
   },
   {
-    year: 2002,
+    year: 1991,
     position: 'Head of international and treasury departments',
     company: 'Bank "Ukraine", Bank PEKAO(Ukraine)',
     experience:
