540b6449dba3436eb5e90dba2c0a7acbb12ce177
Volodymyr <volodymyrquo@gmail.com>
2020-12-13 09:16:48 +0200

    changes in resume

--- src/assets/utilities/workingExperience.js
+++ src/assets/utilities/workingExperience.js
@@ -14,16 +14,15 @@ export const workingExperience = [
   },
   {
     year: 2011,
-    position:
-      'Head of international and treasury departments, regional manager of business development',
-    company: 'different banks ',
+    position: 'Chief of regional business development',
+    company: 'INDEX Bank, Delta Bank, VAB Bank',
     experience:
       'In each position I started from scratch to develop and establish business procedures, to form the corresponding divisions and to make them perfect',
   },
   {
-    year: 2017,
-    position: 'mern full stack',
-    company: 'company name where worked ',
+    year: 2002,
+    position: 'Head of international and treasury departments',
+    company: 'Bank "Ukraine", Bank PEKAO(Ukraine)',
     experience:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui totam omnis quisquam perspiciatis alias voluptatibus fugiatvelit ea reprehenderit harum.',
   },
