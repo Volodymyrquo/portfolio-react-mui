84ef231823fb7290a0412c422f782700306bb11c
Volodymyr <volodymyrquo@gmail.com>
2020-12-13 07:44:55 +0200

    some changes in resume

--- src/assets/utilities/workingExperience.js
+++ src/assets/utilities/workingExperience.js
@@ -1,20 +1,19 @@
 export const workingExperience = [
   {
-    year: 2020,
-    position: 'react js',
-    company: 'company name where worked ',
-    experience:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui totam omnis quisquam perspiciatis alias voluptatibus fugiatvelit ea reprehenderit harum.',
-  },
-  {
     year: 2019,
-    position: 'javascript full stack',
-    company: 'company name where worked ',
+    position: 'react js',
+    company: 'Self-employed   ',
     experience:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui totam omnis quisquam perspiciatis alias voluptatibus fugiatvelit ea reprehenderit harum.',
+      'I have been developing some React projects like "The social network", "Phone store e-commerce". These projects use technologies: ReactJS, Material-UI, Redux, Redux-Thunk, Redux-Form, Hooks, Axios, TDD and more',
   },
   {
-    year: 2018,
+    year: 2011,
+    position: ' Head of Economic Planning Department ',
+    company: 'JSC Volyngas ',
+    experience: 'Economic planning, budgeting, pricing',
+  },
+  {
+    year: 2011,
     position: 'redux',
     company: 'company name where worked ',
     experience:
